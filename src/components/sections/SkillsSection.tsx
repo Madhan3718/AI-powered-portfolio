71126560eafb4bd6c53269685492ff5549c0b0b2
B MADAN KUMAR <2020cse.r28@svec.edu.in>
2025-08-04 17:13:57 +0530

    Updated SkillsSection.tsx

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -92,7 +92,7 @@ const SkillsSection: React.FC<SkillsSectionProps> = ({ onChatSubmit }) => {
         { name: 'DAX', icon: MessageSquare },
         { name: 'Data Cleaning', icon: Palette },
         { name: 'Machine Learning', icon: Search },
-        { name: 'Python Libraries(Numpy, Pandas, Matplotlib, Scikit-Learn)', icon: Zap },
+        { name: 'Python Libraries(Numpy, Pandas, Matplotlib, Scikit-Learn)', icon: Book },
         { name: 'Orchestration', icon: Zap }
       ]
     }
