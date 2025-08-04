6f0d76dbfdfca2cd4db62044c7ff973d0b2ae8cf
B MADAN KUMAR <2020cse.r28@svec.edu.in>
2025-08-04 17:08:28 +0530

    Updated SkillsSection.tsx

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -51,7 +51,7 @@ const SkillsSection: React.FC<SkillsSectionProps> = ({ onChatSubmit }) => {
         { name: 'PowerBI', icon: Monitor },
         { name: 'Excel', icon: Table },
         { name: 'Google Cloud Platform', icon: Cloud },
-        { name: 'Azure Data Studio', icon: CloudCog },
+        { name: 'Azure Data Studio', icon: Cloud },
         { name: 'GitHub', icon: GitBranch },
         { name: 'Cursor', icon: Cloud },
         { name: 'Postman', icon: Cloud },
